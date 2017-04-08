fe7587f8c0e9221c47c37a20121ff7fcda5c2ded
GaganTut <gstut@live.com>
2017-04-08 09:20:52 -1000

    Created vehicle class

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -939,7 +939,12 @@ class Animal {
  * @param {string} make The vehicle's make
  * @param {string} model The vehicle's model
  */
-
+class Vehicle {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+}
 
 /**
  * Step 52
