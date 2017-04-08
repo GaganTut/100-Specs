530c5049b637f55bae1d63470f222218cde2d2b7
GaganTut <gstut@live.com>
2017-04-07 18:03:05 -1000

    Passed test to declare unicorn

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1,11 +1,11 @@
+/*jshint esversion: 6*/
 /* Step 1
  *
  * Declare a variable named "unicorn"
  * then set it's value to null;
  *
  */
-
-
+ let unicorn = null;
 /* Step 2
  *
  * Declare a variable named "videogame_system"
